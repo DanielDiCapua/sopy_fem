b6b3c5be3b6c7f7e7e3a51e2d27fec60547bd5c3
DanielDiCapua <36840530+DanielDiCapua@users.noreply.github.com>
2020-10-19 14:09:32 +0200

    Correction in setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-from distutils.core import setup
+from setuptools import setup
 setup(
   name = 'sopy_fem',
   packages = ['sopy_fem'], # this must be the same as the name above
-  version = '0.1.0',
+  version = '0.1.1',
   description = 'An academic program for finite elments analysis of 2D solids and structures',
   author = 'Daniel Di Capua',
   author_email = 'dicapua67@gmail.com',
   url = 'https://github.com/DanielDiCapua/sopy_fem.git', # use the URL to the github repo
-  download_url = 'https://github.com/DanielDiCapua/sopy_fem.git/tarball/0.1.0',
+  download_url = 'https://github.com/DanielDiCapua/sopy_fem.git/tarball/0.1.1',
   keywords=['developing', 'example'],
   data_files=[('Examples',
     ['sopy_fem/Examples/dynamics_TRUSS02/data.json',
