755e7e067e6c28d6b4e8d691e1ea6faf119b724e
daniel.di.capua <dicapua@cimne.upc.edu>
2020-10-20 13:19:02 +0200

    Inclusion of package_data

--- setup.py
+++ setup.py
@@ -1,21 +1,35 @@
 from distutils.core import setup
+import setuptools
+
 setup(
   name = 'sopy_fem',
-  packages = ['sopy_fem'], # this must be the same as the name above
-  version = '0.1.1',
+  #packages = ['sopy_fem'], # this must be the same as the name above
+  packages= setuptools.find_packages(),
+  version = '0.1.2',
   description = 'An academic program for finite elments analysis of 2D solids and structures',
   author = 'Daniel Di Capua',
   author_email = 'dicapua67@gmail.com',
   url = 'https://github.com/DanielDiCapua/sopy_fem.git', # use the URL to the github repo
-  download_url = 'https://github.com/DanielDiCapua/sopy_fem.git/tarball/0.1.1',
+  download_url = 'https://github.com/DanielDiCapua/sopy_fem.git/tarball/0.1.2',
   keywords=['developing', 'example'],
-  data_files=[('Examples',
-    ['sopy_fem/Examples/dynamics_TRUSS02/data.json',
-    'sopy_fem/Examples/electrical_BR02/data.json',
-    'sopy_fem/Examples/mechanics_BR02/data.json',
-    'sopy_fem/Examples/mechanics_QU04/data.json',
-    'sopy_fem/Examples/mechanics_TR03/data.json',
-    'sopy_fem/Examples/structural_TRUSS02/data.json',
-    'sopy_fem/Examples/thermal_BR02/data.json'])],
+  # data_files=[('Examples',
+  #   ['sopy_fem/Examples/dynamics_TRUSS02/data.json',
+  #   'sopy_fem/Examples/electrical_BR02/data.json',
+  #   'sopy_fem/Examples/mechanics_BR02/data.json',
+  #   'sopy_fem/Examples/mechanics_QU04/data.json',
+  #   'sopy_fem/Examples/mechanics_TR03/data.json',
+  #   'sopy_fem/Examples/structural_TRUSS02/data.json',
+  #   'sopy_fem/Examples/thermal_BR02/data.json'])],
+  include_package_data=True,
+  package_dir={'sopy_fem': 'sopy_fem/sopy_fem'},
+  package_data={'sopy_fem': [
+    'Examples/dynamics_TRUSS02/data.json',
+    'Examples/electrical_BR02/data.json',
+    'Examples/mechanics_BR02/data.json',
+    'Examples/mechanics_QU04/data.json',
+    'Examples/mechanics_TR03/data.json',
+    'Examples/structural_TRUSS02/data.json',
+    'Examples/thermal_BR02/data.json'
+    ]},
   classifiers = [],
 )
